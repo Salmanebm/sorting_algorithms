ed67eb752484fa28c6eb2b6830fa301ed3387e71
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-21 19:12:35 +0300

    Modifying Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,13 +22,20 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
+				swab(array + i, array + i + 1);
 				swapped = 1;
 				print_array(array, size);
 			}
 		}
 		
 	}
+}
+
+void swab(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
