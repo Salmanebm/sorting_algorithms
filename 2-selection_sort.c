7b6f9fa752c2a308188a2d27266b75e0b964c48e
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-10-21 12:41:49 -0700

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-
+/**
+ * selection_sort - Sorts an array of integers using the selection sort.
+ * @array: Pointer to the array to be sorted.
+ * @size: Number of elements in the array.
+ */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
 
 	for (i = 0; i < size - 1; i++)
 	{
-        	min_idx = i;
-        	for (j = i+1; j < size; j++)
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
-		if(min_idx != i)
+		if (min_idx != i)
 		{
 			swap(&array[min_idx], &array[i]);
 			print_array(array, size);
--- swap.c
+++ swap.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-
+/**
+ * swap - Swaps two integer values.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ *
+ * This function swaps the values pointed to by 'a' and 'b'.
+ */
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
