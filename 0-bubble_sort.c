740bd4c80a987d61168381e0843d0079751ab73a
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-21 19:07:37 +0300

    Fixing Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,28 +9,26 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int sort_check = 1, tmp;
+	int swapped = 1, tmp;
 	size_t i;
 
-	while (sort_check == 1)
+	if (array == NULL || size < 2)
+		return;
+
+	while (swapped == 1)
 	{
-Here:
-		i = 0;
-		while (i < size - 1)
+		swapped = 0;
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				tmp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = tmp;
+				swapped = 1;
 				print_array(array, size);
-				goto Here;
 			}
-			else
-			{
-				sort_check = 0;
-			}
-			i++;
 		}
+		
 	}
 }
