2144a0c6d912be727ddbf34899df30b68c9b47d7
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-24 01:52:37 -0700

    Adding swap

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,6 +3,20 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps two integer values.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ *
+ * This function swaps the values pointed to by 'a' and 'b'.
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * bubble_sort - Sorts an array of integers Bubble Sort
  *
  * @array: The array to be sorted.
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
+
+/**
+ * swap - Swaps two integer values.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ *
+ * This function swaps the values pointed to by 'a' and 'b'.
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 /**
  * selection_sort - Sorts an array of integers using the selection sort.
  * @array: Pointer to the array to be sorted.
