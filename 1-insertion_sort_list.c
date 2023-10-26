5fc5f3261634cd074f149c57c0009f948fb0c1a2
Salmane Ben Messaoud <salmanesbm@gmail.com>
2023-10-26 14:41:50 -0700

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,17 +9,17 @@
  */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-        (*n1)->next = n2->next;
-        if (n2->next != NULL)
-                n2->next->prev = *n1;
-        n2->prev = (*n1)->prev;
-        n2->next = *n1;
-        if ((*n1)->prev != NULL)
-                (*n1)->prev->next = n2;
-        else
-                *h = n2;
-        (*n1)->prev = n2;
-        *n1 = n2->prev;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
